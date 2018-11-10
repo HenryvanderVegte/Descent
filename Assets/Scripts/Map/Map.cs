b37f59f31d77c1fd516fed1807d429336a244696
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2018-11-10 15:39:38 +0100

    added prefabs, changed map reading and tile status

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    private int[,] TileValues;
+
+    void Start()
+    {
+        string contents = File.ReadAllText(@"C:\Users\Henry\Desktop\Descent\Assets\Maps\Map01.json");
+    }
+
+    private void DeserializeMap(string json)
+    {
+        MapJSON map = JsonConvert.DeserializeObject<MapJSON>(json);
+
+        int x = map.mapRows[0].row.Split(',').Length;
+        int y = map.mapRows.Length;
+        TileValues = new int[x, y];
+
+        for(int i = 0; i < x; i++)
+        {
+            string[] rowValues = map.mapRows[i].row.Split(',');
+            for (int j = 0; j < y; j++)
+            {
+                int value;
+                TileValues[i, j] = (int.TryParse(rowValues[j], out value) ? value : 0);
+            }
+        }
+    }
+}
+
+public class MapJSON
+{
+    public MapRow[] mapRows { get; set; }
+}
+
+public class MapRow
+{
+
+    public string row { get; set; }
+}
--- Assets/Scripts/Map/TileStatus.cs
+++ Assets/Scripts/Map/TileStatus.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TileStatus : MonoBehaviour {
-
+   
     public static TileStatus Empty = new TileStatus(-1, true);
 
     public static TileStatus Ground = new TileStatus(1, false);
@@ -20,13 +20,48 @@ public class TileStatus : MonoBehaviour {
 
     public static TileStatus Staircase = new TileStatus(1, false);
 
-    private int MovementToEnter { get; }
+    private int MovementToEnter;
 
-    private bool BlocksLineOfSight { get; }
+    private bool BlocksLineOfSight;
 
     private TileStatus(int movementToEnter, bool blocksLineOfSight)
     {
         this.MovementToEnter = movementToEnter;
         this.BlocksLineOfSight = blocksLineOfSight;
+    }
+
+    public int GetMovementToEnter()
+    {
+        return MovementToEnter;
+    }
+
+    public bool IsBlockingLineOfSight()
+    {
+        return BlocksLineOfSight;
+    }
+
+    public static TileStatus GetTileStatusByID(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                return TileStatus.Empty;
+            case 1:
+                return TileStatus.Ground;
+            case 2:
+                return TileStatus.Water;
+            case 3:
+                return TileStatus.Rubble;
+            case 4:
+                return TileStatus.Encounters;
+            case 5:
+                return TileStatus.GlyphDeactivated;
+            case 6:
+                return TileStatus.GlyphActivated;
+            case 7:
+                return TileStatus.Staircase;
+            default:
+                return TileStatus.Empty;
+        }
     }
 }
