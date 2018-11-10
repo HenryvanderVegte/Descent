727fc88197a9edd81756c4a744bd0118f7bf5152
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2018-11-10 13:54:56 +0100

    add tileStatus

---
+++ Assets/Scripts/Map/TileStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileStatus : MonoBehaviour {
+
+    public static TileStatus Empty = new TileStatus(-1, true);
+
+    public static TileStatus Ground = new TileStatus(1, false);
+
+    public static TileStatus Water = new TileStatus(-1, false);
+
+    public static TileStatus Rubble = new TileStatus(-1, true);
+
+    public static TileStatus Encounters = new TileStatus(1, true);
+
+    public static TileStatus GlyphDeactivated = new TileStatus(1, false);
+
+    public static TileStatus GlyphActivated = new TileStatus(1, false);
+
+    public static TileStatus Staircase = new TileStatus(1, false);
+
+    private int MovementToEnter { get; }
+
+    private bool BlocksLineOfSight { get; }
+
+    private TileStatus(int movementToEnter, bool blocksLineOfSight)
+    {
+        this.MovementToEnter = movementToEnter;
+        this.BlocksLineOfSight = blocksLineOfSight;
+    }
+}
